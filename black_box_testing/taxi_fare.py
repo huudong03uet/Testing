49e751b0b0cdb5b1e84d4ae653634f4a894a5f2e
huudong03uet <huudong03uet@gmail.com>
2024-09-30 10:29:17 +0700

    black box testing

---
+++ black_box_testing/taxi_fare.py
@@ -0,0 +1,35 @@
+class TaxiFareCalculator:
+    def __init__(self):
+        self.opening_fare = 30000  
+        self.fare_up_to_20km = 20000  
+        self.fare_above_21km = 10000  
+
+    def calculate_fare(self, distance: float, num_people: int) -> float:
+        if distance < 0 or distance > 200:
+            raise ValueError("Khoảng cách phải từ 0 đến 200 km")
+
+        if num_people < 1 or num_people > 6:
+            raise ValueError("Số lượng người phải lớn hơn 1 và dưới 6 người")
+        
+        fare_for_one = self.opening_fare
+
+        if distance > 1:
+            if distance <= 20:
+                fare_for_one += (distance - 1) * self.fare_up_to_20km
+            else:
+                fare_for_one += 19 * self.fare_up_to_20km  
+                fare_for_one += (distance - 20) * self.fare_above_21km  
+
+        fare_taxi = 0
+
+        if num_people > 1:
+            fare_taxi = fare_for_one + fare_for_one * 0.2 * (num_people - 1)
+        else:
+            fare_taxi = fare_for_one
+        return fare_taxi
+    
+
+if __name__ == "__main__":
+    calculator = TaxiFareCalculator()
+    fare = calculator.calculate_fare(100, 2)
+    print(fare)
---
+++ black_box_testing/test_taxi_fare.py
@@ -0,0 +1,76 @@
+import pytest
+
+from taxi_fare import TaxiFareCalculator
+
+class TestTaxiFareCalculator:
+    def setup_method(self):
+        self.taxi_fare_calculator = TaxiFareCalculator()
+    
+    # Kiem thu bien
+    def test_tc1(self):
+        assert self.taxi_fare_calculator.calculate_fare(100, 1) == 1210000
+    
+    def test_tc2(self):
+        assert self.taxi_fare_calculator.calculate_fare(100, 2) == 1452000
+    
+    def test_tc3(self):
+        assert self.taxi_fare_calculator.calculate_fare(100, 3) == 1694000
+    
+    def test_tc4(self):
+        assert self.taxi_fare_calculator.calculate_fare(100, 4) == 1936000
+    
+    def test_tc5(self):
+        assert self.taxi_fare_calculator.calculate_fare(100, 5) == 2178000
+    
+    def test_tc6(self):
+        assert self.taxi_fare_calculator.calculate_fare(0, 3) == 42000
+    
+    def test_tc7(self):
+        assert self.taxi_fare_calculator.calculate_fare(0.1, 3) == 42000
+    
+    def test_tc8(self):
+        assert self.taxi_fare_calculator.calculate_fare(199.9, 3) == 3092600
+    
+    def test_tc9(self):
+        assert self.taxi_fare_calculator.calculate_fare(200, 3) == 3094000
+
+    # Kiem thu bang quyet dinh
+    def test_tc10(self):
+        with pytest.raises(ValueError):
+            self.taxi_fare_calculator.calculate_fare(100, -1)
+    def test_tc11(self):
+        with pytest.raises(ValueError):
+            self.taxi_fare_calculator.calculate_fare(-1, 1)
+
+    def test_tc12(self):
+        assert self.taxi_fare_calculator.calculate_fare(0, 1) == 30000
+
+    def test_tc13(self):
+        assert self.taxi_fare_calculator.calculate_fare(10, 1) == 210000
+
+    def test_tc14(self):
+        assert self.taxi_fare_calculator.calculate_fare(100, 1) == 1210000
+
+    def test_tc15(self):
+        with pytest.raises(ValueError):
+            self.taxi_fare_calculator.calculate_fare(300, 1)
+
+    def test_tc16(self):
+        with pytest.raises(ValueError):
+            self.taxi_fare_calculator.calculate_fare(100, 6)
+
+    def test_tc17(self):
+        with pytest.raises(ValueError):
+            self.taxi_fare_calculator.calculate_fare(-1, 2)
+
+    def test_tc18(self):
+        assert self.taxi_fare_calculator.calculate_fare(0, 2) == 36000
+    def test_tc19(self):
+        assert self.taxi_fare_calculator.calculate_fare(10, 2) == 252000
+
+    def test_tc20(self):
+        assert self.taxi_fare_calculator.calculate_fare(100, 2) == 1452000
+
+    def test_tc21(self):
+        with pytest.raises(ValueError):
+            self.taxi_fare_calculator.calculate_fare(300, 2)
